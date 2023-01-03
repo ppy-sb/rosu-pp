688a46bddfed826af068d0e76bf3df1f973ffcc0
Usagi no Niku <chenbohan911@163.com>
2023-01-04 02:03:48 +0800

    Update aim.rs

--- src/osu/skills/aim.rs
+++ src/osu/skills/aim.rs
@@ -282,7 +282,7 @@ impl AimEvaluator {
                 single_spacing_threshold.min(travel_dist + osu_curr_obj.dists.min_jump_dist);
             // Maybe this can show how the aim looks like stream arrangements
             let abstract_speed_value = (dist / single_spacing_threshold).powf(3.5) / strain_time;
-            let speed_nerf = -19.73 * abstract_speed_value + 0.99;
+            let speed_nerf = -25.92 * abstract_speed_value + 1.02;
             aim_strain *= speed_nerf;
             println!("AIM: {}, ASV: {}, AAB: {}", aim_strain,  abstract_speed_value, acute_angle_bonus)
         }
