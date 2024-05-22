e60a634f0f33eb65e43add4b7654a652904d0825
Usagi no Niku <chenbohan911@163.com>
2024-05-22 11:27:28 +0800

    feature: remove combo scaling for relax

--- src/osu/performance/mod.rs
+++ src/osu/performance/mod.rs
@@ -599,7 +599,10 @@ impl OsuPerformanceInner {
                     .powf(growth_power);
         }
 
-        aim_value *= self.get_combo_scaling_factor();
+        // * Remove combo scaling for relax.
+        if !self.mods.rx() {
+            aim_value *= self.get_combo_scaling_factor();
+        }
 
         let lowar_factor_basis = if self.mods.rx() { 0.025 } else { 0.05 };
         let ar_factor = if self.attrs.ar > 10.33 {
