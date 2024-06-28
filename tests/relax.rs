1d08d3d0578778ae82b6d61f0a8c886767629177
Usagi no Niku <chenbohan911@163.com>
2024-06-28 17:51:59 +0800

    improve: more straight format & add readme

--- tests/relax.rs
+++ tests/relax.rs
@@ -1,57 +1,87 @@
-#[macro_use(lazy_static)]
-extern crate lazy_static;
-
-use std::{collections::HashMap, sync::Mutex};
-
 use rosu_pp::{Beatmap, Difficulty};
 
-const TEST_CASES: [(&str, u32, Difficulty); 23] = [
-    ("Sunglow", 2486881, Difficulty::new().mods(200)), // HDDTRX
-    ("Sidetracked Day", 1537566, Difficulty::new().mods(200)), // HDDTRX
-    ("Team Magma", 2097898, Difficulty::new().mods(200)), // HDDTRX
-    ("Kimi no Kioku", 1045757, Difficulty::new().mods(200)), // HDDTRX
-    ("ROR", 869222, Difficulty::new().mods(200)),      // HDDTRX
-    ("Euphoria", 1861487, Difficulty::new().mods(144)), // HRRX
-    ("Oshama Scramble", 1972244, Difficulty::new().mods(144)), // HRRX
-    ("The Sun The Moon", 1949106, Difficulty::new().mods(200)), // HRRX
-    ("Roar of The Jungle", 3306465, Difficulty::new().mods(200)), // HDDTRX
-    ("Cosmic cortex", 787307, Difficulty::new().mods(200)), // HDDTRX
-    ("Chronostasis", 2874408, Difficulty::new().mods(200)), // HDDTRX
-    ("quaver", 918723, Difficulty::new().mods(200)), // HDDTRX
-    ("raise my sword", 1860169, Difficulty::new().mods(200)), // HDDTRX
-    ("Hardware Store", 1989203, Difficulty::new().mods(216)), // HDHRDTRX
-    ("Songs Compilation", 1849580, Difficulty::new().mods(200)), // HDDTRX
-    ("Shinryu Monogatari", 2818772, Difficulty::new().mods(144)), // HRRX
-    ("THE PLATINUM", 4352925, Difficulty::new().mods(128)), // RX
-    ("Charge-Parity", 4349848, Difficulty::new().mods(128)), // RX
-    ("Fallen Symphony", 4042579, Difficulty::new().mods(128)), // RX
-    ("TERRAFORGE", 4439703, Difficulty::new().mods(128)), // RX
-    ("Lovely Icecream", 1415526, Difficulty::new().mods(144)), // HRRX
-    ("Scarlet Rose", 131564, Difficulty::new().mods(200)), // HDDTRX
-    ("True truly love", 955615, Difficulty::new().mods(128)), // RX
+const TEST_CASES: [(&str, u32, Difficulty, u32); 23] = [
+    ("Sunglow", 2486881, Difficulty::new().mods(200), 1000), // HDDTRX
+    (
+        "Sidetracked Day",
+        1537566,
+        Difficulty::new().mods(200),
+        1100,
+    ), // HDDTRX
+    ("Team Magma", 2097898, Difficulty::new().mods(200), 870), // HDDTRX
+    ("Kimi no Kioku", 1045757, Difficulty::new().mods(200), 750), // HDDTRX
+    ("ROR", 869222, Difficulty::new().mods(200), 1250),      // HDDTRX
+    ("Euphoria", 1861487, Difficulty::new().mods(144), 1000), // HRRX
+    ("Genryuu Kaiko", 433005, Difficulty::new().mods(144), 900), // HRRX
+    (
+        "The Sun The Moon",
+        1949106,
+        Difficulty::new().mods(200),
+        1850,
+    ), // HRRX
+    (
+        "Roar of The Jungle",
+        3306465,
+        Difficulty::new().mods(200),
+        2500,
+    ), // HDDTRX
+    ("Cosmic cortex", 787307, Difficulty::new().mods(200), 1350), // HDDTRX
+    ("Chronostasis", 2874408, Difficulty::new().mods(200), 1950), // HDDTRX
+    ("quaver", 918723, Difficulty::new().mods(200), 1750),   // HDDTRX
+    ("raise my sword", 1860169, Difficulty::new().mods(200), 1350), // HDDTRX
+    ("Hardware Store", 1989203, Difficulty::new().mods(216), 1375), // HDHRDTRX
+    (
+        "Songs Compilation",
+        1849580,
+        Difficulty::new().mods(200),
+        1575,
+    ), // HDDTRX
+    (
+        "Shinryu Monogatari",
+        2818772,
+        Difficulty::new().mods(144),
+        1125,
+    ), // HRRX
+    ("THE PLATINUM", 4352925, Difficulty::new().mods(128), 1350), // RX
+    ("Charge-Parity", 4349848, Difficulty::new().mods(128), 1000), // RX
+    (
+        "Fallen Symphony",
+        4042579,
+        Difficulty::new().mods(128),
+        1000,
+    ), // RX
+    ("TERRAFORGE", 4439703, Difficulty::new().mods(128), 1150), // RX
+    ("Lovely Icecream", 1415526, Difficulty::new().mods(144), 750), // HRRX
+    ("Scarlet Rose", 131564, Difficulty::new().mods(200), 600), // HDDTRX
+    ("True truly love", 955615, Difficulty::new().mods(128), 825), // RX
 ];
-
-lazy_static! {
-    pub static ref PROCEDURE: Mutex<HashMap<String, String>> = Mutex::new(HashMap::new());
-}
 
 #[test]
 fn calculate_batch() {
     println!(
-        "{0: <20} | {1: <10} | {2: <10} | {3: <10}",
-        "title", "pp_value", "procedure_1", "procedure_2"
+        "{0: <20} | {1: <12} | {2: <12} | {3: <10} | {4: <10}",
+        "title", "pp", "weighted_pp", "target_pp", "delta"
     );
-    for (title, beatmap_id, diff) in TEST_CASES {
-        PROCEDURE.lock().unwrap().clear();
+    let weight = get_weight();
+    for (title, beatmap_id, diff, target_pp) in TEST_CASES {
         let map = Beatmap::from_path(format!("./resources/{}.osu", beatmap_id)).unwrap();
         let performance = rosu_pp::Performance::new(diff.calculate(&map));
         let pp_value = performance.accuracy(99.5).calculate().pp();
+        let pp_weighted = pp_value * weight;
         println!(
-            "{0: <20} | {1: <10} | {2: <10} | {3: <10}",
+            "{0: <20} | {1: <12} | {2: <12} | {3: <10} | {4: <10}",
             title,
             format!("{:.2}", pp_value),
-            0,
-            0
+            format!("{:.2}", pp_weighted),
+            format!("{:.2}", target_pp),
+            format!("{:.2}", pp_weighted - target_pp as f64)
         );
     }
+}
+
+fn get_weight() -> f64 {
+    let map = Beatmap::from_path("./resources/2486881.osu").unwrap();
+    let performance = rosu_pp::Performance::new(Difficulty::new().mods(200).calculate(&map));
+    let pp_value = performance.accuracy(99.5).calculate().pp();
+    1000.0 / pp_value
 }
