d749431c2d584fdb07375d49da6d5228fedde924
Usagi no Niku <chenbohan911@163.com>
2025-04-17 00:17:53 +0800

    feat: include RelaxRhythmEvaluator

--- src/osu/difficulty/skills/relax.rs
+++ src/osu/difficulty/skills/relax.rs
@@ -1,4 +1,5 @@
 use std::f64::consts::FRAC_PI_2;
+use std::{cmp, f64::consts::PI};
 
 use crate::{
     any::difficulty::{
@@ -7,7 +8,7 @@ use crate::{
     },
     osu::difficulty::object::OsuDifficultyObject,
     util::{
-        difficulty::{milliseconds_to_bpm, reverse_lerp, smootherstep, smoothstep},
+        difficulty::{logistic, milliseconds_to_bpm, reverse_lerp, smootherstep, smoothstep},
         float_ext::FloatExt,
         strains_vec::StrainsVec,
     },
@@ -20,6 +21,7 @@ define_skill! {
     pub struct Relax: StrainSkill => [OsuDifficultyObject<'a>][OsuDifficultyObject<'a>] {
         include_sliders: bool,
         current_strain: f64 = 0.0,
+        hit_window: f64,
         slider_strains: Vec<f64> = Vec::with_capacity(64), // TODO: use `StrainsVec`?
     }
 }
@@ -47,8 +49,12 @@ impl Relax {
         objects: &[OsuDifficultyObject<'_>],
     ) -> f64 {
         self.current_strain *= strain_decay(curr.delta_time, Self::STRAIN_DECAY_BASE);
-        self.current_strain += AimEvaluator::evaluate_diff_of(curr, objects, self.include_sliders)
-            * Self::SKILL_MULTIPLIER;
+        self.current_strain += RelaxAimEvaluator::evaluate_diff_of(
+            curr,
+            objects,
+            self.hit_window,
+            self.include_sliders,
+        ) * Self::SKILL_MULTIPLIER;
 
         if curr.base.is_slider() {
             self.slider_strains.push(self.current_strain);
@@ -89,9 +95,9 @@ impl Relax {
 
 impl OsuStrainSkill for Relax {}
 
-struct AimEvaluator;
+struct RelaxAimEvaluator;
 
-impl AimEvaluator {
+impl RelaxAimEvaluator {
     const WIDE_ANGLE_MULTIPLIER: f64 = 1.5;
     const ACUTE_ANGLE_MULTIPLIER: f64 = 2.6;
     const SLIDER_MULTIPLIER: f64 = 1.35;
@@ -102,6 +108,7 @@ impl AimEvaluator {
     fn evaluate_diff_of<'a>(
         curr: &'a OsuDifficultyObject<'a>,
         diff_objects: &'a [OsuDifficultyObject<'a>],
+        hit_window: f64,
         with_slider_travel_dist: bool,
     ) -> f64 {
         let osu_curr_obj = curr;
@@ -274,6 +281,11 @@ impl AimEvaluator {
             aim_strain += slider_bonus * Self::SLIDER_MULTIPLIER;
         }
 
+        // * If the distance is small enough, we want to buff the rhythm complexity.
+        if osu_curr_obj.lazy_jump_dist < 350.0 {
+            aim_strain *= RelaxRhythmEvaluator::evaluate_diff_of(curr, diff_objects, hit_window);
+        }
+
         aim_strain
     }
 
@@ -284,4 +296,264 @@ impl AimEvaluator {
     const fn calc_acute_angle_bonus(angle: f64) -> f64 {
         smoothstep(angle, f64::to_radians(140.0), f64::to_radians(40.0))
     }
+}
+
+struct RelaxRhythmEvaluator;
+
+impl RelaxRhythmEvaluator {
+    const HISTORY_TIME_MAX: u32 = 5 * 1000; // 5 seconds
+    const HISTORY_OBJECTS_MAX: usize = 32;
+    const RHYTHM_OVERALL_MULTIPLIER: f64 = 0.95;
+    const RHYTHM_RATIO_MULTIPLIER: f64 = 12.0;
+
+    #[allow(clippy::too_many_lines)]
+    fn evaluate_diff_of<'a>(
+        curr: &'a OsuDifficultyObject<'a>,
+        diff_objects: &'a [OsuDifficultyObject<'a>],
+        hit_window: f64,
+    ) -> f64 {
+        if curr.base.is_spinner() {
+            return 0.0;
+        }
+
+        let mut rhythm_complexity_sum = 0.0;
+
+        let delta_difference_eps = hit_window * 0.3;
+
+        let mut island = RhythmIsland::new(delta_difference_eps);
+        let mut prev_island = RhythmIsland::new(delta_difference_eps);
+
+        // * we can't use dictionary here because we need to compare island with a tolerance
+        // * which is impossible to pass into the hash comparer
+        let mut island_counts = Vec::<IslandCount>::new();
+
+        // * store the ratio of the current start of an island to buff for tighter rhythms
+        let mut start_ratio = 0.0;
+
+        let mut first_delta_switch = false;
+
+        let historical_note_count = cmp::min(curr.idx, Self::HISTORY_OBJECTS_MAX);
+
+        let mut rhythm_start = 0;
+
+        while curr
+            .previous(rhythm_start, diff_objects)
+            .filter(|prev| {
+                rhythm_start + 2 < historical_note_count
+                    && curr.start_time - prev.start_time < f64::from(Self::HISTORY_TIME_MAX)
+            })
+            .is_some()
+        {
+            rhythm_start += 1;
+        }
+
+        if let Some((mut prev_obj, mut last_obj)) = curr
+            .previous(rhythm_start, diff_objects)
+            .zip(curr.previous(rhythm_start + 1, diff_objects))
+        {
+            // * we go from the furthest object back to the current one
+            for i in (1..=rhythm_start).rev() {
+                let Some(curr_obj) = curr.previous(i - 1, diff_objects) else {
+                    break;
+                };
+
+                // * scales note 0 to 1 from history to now
+                let time_decay = (f64::from(Self::HISTORY_TIME_MAX)
+                    - (curr.start_time - curr_obj.start_time))
+                    / f64::from(Self::HISTORY_TIME_MAX);
+                let note_decay = (historical_note_count - i) as f64 / historical_note_count as f64;
+
+                // * either we're limited by time or limited by object count.
+                let curr_historical_decay = note_decay.min(time_decay);
+
+                let curr_delta = curr_obj.strain_time;
+                let prev_delta = prev_obj.strain_time;
+                let last_delta = last_obj.strain_time;
+
+                // * calculate how much current delta difference deserves a rhythm bonus
+                // * this function is meant to reduce rhythm bonus for deltas that are multiples of each other (i.e 100 and 200)
+                let delta_difference_ratio =
+                    prev_delta.min(curr_delta) / prev_delta.max(curr_delta);
+                let curr_ratio = 1.0
+                    + Self::RHYTHM_RATIO_MULTIPLIER
+                        * (PI / delta_difference_ratio).sin().powf(2.0).min(0.5);
+
+                // reduce ratio bonus if delta difference is too big
+                let fraction = (prev_delta / curr_delta).max(curr_delta / prev_delta);
+                let fraction_multiplier = (2.0 - fraction / 8.0).clamp(0.0, 1.0);
+
+                let window_penalty = (((prev_delta - curr_delta).abs() - delta_difference_eps)
+                    .max(0.0)
+                    / delta_difference_eps)
+                    .min(1.0);
+
+                let mut effective_ratio = window_penalty * curr_ratio * fraction_multiplier;
+
+                if first_delta_switch {
+                    // Keep in-sync with lazer
+                    #[allow(clippy::if_not_else)]
+                    if (prev_delta - curr_delta).abs() < delta_difference_eps {
+                        // * island is still progressing
+                        island.add_delta(curr_delta as i32);
+                    } else {
+                        // * bpm change is into slider, this is easy acc window
+                        if curr_obj.base.is_slider() {
+                            effective_ratio *= 0.125;
+                        }
+
+                        // * bpm change was from a slider, this is easier typically than circle -> circle
+                        // * unintentional side effect is that bursts with kicksliders at the ends might have lower difficulty than bursts without sliders
+                        if prev_obj.base.is_slider() {
+                            effective_ratio *= 0.3;
+                        }
+
+                        // * repeated island polarity (2 -> 4, 3 -> 5)
+                        if island.is_similar_polarity(&prev_island) {
+                            effective_ratio *= 0.5;
+                        }
+
+                        // * previous increase happened a note ago, 1/1->1/2-1/4, dont want to buff this.
+                        if last_delta > prev_delta + delta_difference_eps
+                            && prev_delta > curr_delta + delta_difference_eps
+                        {
+                            effective_ratio *= 0.125;
+                        }
+
+                        // * repeated island size (ex: triplet -> triplet)
+                        // * TODO: remove this nerf since its staying here only for balancing purposes because of the flawed ratio calculation
+                        if prev_island.delta_count == island.delta_count {
+                            effective_ratio *= 0.5;
+                        }
+
+                        if let Some(island_count) = island_counts
+                            .iter_mut()
+                            .find(|entry| entry.island == island)
+                            .filter(|entry| !entry.island.is_default())
+                        {
+                            // * only add island to island counts if they're going one after another
+                            if prev_island == island {
+                                island_count.count += 1;
+                            }
+
+                            // * repeated island (ex: triplet -> triplet)
+                            let power = logistic(f64::from(island.delta), 58.33, 0.24, Some(2.75));
+                            effective_ratio *= (3.0 / island_count.count as f64)
+                                .min((island_count.count as f64).recip().powf(power));
+                        } else {
+                            island_counts.push(IslandCount { island, count: 1 });
+                        }
+
+                        // * scale down the difficulty if the object is doubletappable
+                        let doubletapness = prev_obj.get_doubletapness(Some(curr_obj), hit_window);
+                        effective_ratio *= 1.0 - doubletapness * 0.75;
+
+                        rhythm_complexity_sum +=
+                            (effective_ratio * start_ratio).sqrt() * curr_historical_decay;
+
+                        start_ratio = effective_ratio;
+
+                        prev_island = island;
+
+                        // * we're slowing down, stop counting
+                        if prev_delta + delta_difference_eps < curr_delta {
+                            // * if we're speeding up, this stays true and we keep counting island size.
+                            first_delta_switch = false;
+                        }
+
+                        island =
+                            RhythmIsland::new_with_delta(curr_delta as i32, delta_difference_eps);
+                    }
+                } else if prev_delta > curr_delta + delta_difference_eps {
+                    // * we're speeding up.
+                    // * Begin counting island until we change speed again.
+                    first_delta_switch = true;
+
+                    // * bpm change is into slider, this is easy acc window
+                    if curr_obj.base.is_slider() {
+                        effective_ratio *= 0.6;
+                    }
+
+                    // * bpm change was from a slider, this is easier typically than circle -> circle
+                    // * unintentional side effect is that bursts with kicksliders at the ends might have lower difficulty than bursts without sliders
+                    if prev_obj.base.is_slider() {
+                        effective_ratio *= 0.6;
+                    }
+
+                    start_ratio = effective_ratio;
+
+                    island = RhythmIsland::new_with_delta(curr_delta as i32, delta_difference_eps);
+                }
+
+                last_obj = prev_obj;
+                prev_obj = curr_obj;
+            }
+        }
+
+        // * produces multiplier that can be applied to strain. range [1, infinity) (not really though)
+        (4.0 + rhythm_complexity_sum * Self::RHYTHM_OVERALL_MULTIPLIER).sqrt() / 2.0
+    }
+}
+
+#[derive(Copy, Clone)]
+struct RhythmIsland {
+    delta_difference_eps: f64,
+    delta: i32,
+    delta_count: i32,
+}
+
+const MIN_DELTA_TIME: i32 = 25;
+
+// Compile-time check in case `OsuDifficultyObject::MIN_DELTA_TIME` changes
+// but we forget to update this value.
+const _: [(); 0 - !{ MIN_DELTA_TIME - OsuDifficultyObject::MIN_DELTA_TIME as i32 == 0 } as usize] =
+    [];
+
+impl RhythmIsland {
+    const fn new(delta_difference_eps: f64) -> Self {
+        Self {
+            delta_difference_eps,
+            delta: 0,
+            delta_count: 0,
+        }
+    }
+
+    fn new_with_delta(delta: i32, delta_difference_eps: f64) -> Self {
+        Self {
+            delta_difference_eps,
+            delta: delta.max(MIN_DELTA_TIME),
+            delta_count: 1,
+        }
+    }
+
+    fn add_delta(&mut self, delta: i32) {
+        if self.delta == i32::MAX {
+            self.delta = delta.max(MIN_DELTA_TIME);
+        }
+
+        self.delta_count += 1;
+    }
+
+    const fn is_similar_polarity(&self, other: &Self) -> bool {
+        // * TODO: consider islands to be of similar polarity only if they're having the same average delta (we don't want to consider 3 singletaps similar to a triple)
+        // *       naively adding delta check here breaks _a lot_ of maps because of the flawed ratio calculation
+        self.delta_count % 2 == other.delta_count % 2
+    }
+
+    fn is_default(&self) -> bool {
+        self.delta_difference_eps.abs() < f64::EPSILON
+            && self.delta == i32::MAX
+            && self.delta_count == 0
+    }
+}
+
+impl PartialEq for RhythmIsland {
+    fn eq(&self, other: &Self) -> bool {
+        f64::from((self.delta - other.delta).abs()) < self.delta_difference_eps
+            && self.delta_count == other.delta_count
+    }
+}
+
+struct IslandCount {
+    island: RhythmIsland,
+    count: usize,
 }
